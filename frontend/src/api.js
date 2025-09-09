dc892997b66d1068a0c9ee2218358360dd741f34
Dorothy Akoth <79142184+Dorothy2020@users.noreply.github.com>
2025-09-09 11:36:08 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://staging.kaakazini.com/api/';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://staging.kaakazini.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
