416a4449bd2db46cec06fb4ef19a15c99c69202e
Dorothy Akoth <wangarraakoth@gmail.com>
2025-09-20 05:47:52 +0300

    feat:add forgotpasswordBackend

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,6 +7,8 @@ from .views import RegisterView, GoogleLoginView
 from .views import AdminLoginAPIView, LoginAPIView
 from .views import ProfileView
 from .views import ClientSignupView, ClientLoginView
+from .views import PasswordResetView, PasswordResetConfirmView
+
 
 # from .token import CustomTokenObtainPairView
 
@@ -20,19 +22,16 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('google-login/', GoogleLoginView.as_view(), name='google_login'),
-    
-
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 
     
 
     # admin
-
     path('admin-login/', AdminLoginAPIView.as_view(), name='admin-login'),
 
-    
     # Hiring Clients
-
     path('client-signup/', ClientSignupView.as_view(), name='client-signup'),
     path('client-login/', ClientLoginView.as_view(), name='client-login'),
 ]
