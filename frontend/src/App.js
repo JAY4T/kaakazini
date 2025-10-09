04b9f6840747991aa22ab68df3a21032f64eb7f2
Dorothy Akoth <wangarraakoth@gmail.com>
2025-10-09 20:37:07 +0300

    added imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import SignupPage from './pages/SignupPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
-
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import PublicProfile from './pages/PublicProfile';
 import Navbar from './components/Navbar';
 import CartPage from './pages/CartPage';
-import WoodworkingCategoryPage from './pages/WoodworkingCategoryPage'; // woodworking products
-import MetalworkingCategoryPage from './pages/MetalworkingCategoryPage'; // metalworking products
-import TextileCategoryPage from './pages/TextileCategoryPage'; // metalworking products
+import WoodworkingCategoryPage from './pages/WoodworkingCategoryPage';
+import MetalworkingCategoryPage from './pages/MetalworkingCategoryPage';
+import TextileCategoryPage from './pages/TextileCategoryPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CraftsmenListPage from './pages/CraftsmenListPage';
 import CraftsmanProfilePage from './pages/CraftsmanProfilePage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import AdminDashboardPage from './pages/AdminDashboardPage'; 
 import AdminLoginPage from './pages/AdminLoginPage';
-import HireSignup from './pages/HireSignUpPage';
+import HireSignUp from './pages/HireSignUpPage';
 import HireCraftsmanPage from './pages/HireCraftsmanPage';
 import TermsConditionPage from './pages/TermsConditionPage';
 import HireLogin from './pages/HireLoginPage';
 import ServicesPage from './pages/ServicesPage';
 import ResetPasswordPage from "./pages/ResetPasswordPage";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import NoShopPage from "./pages/NoShopPage"; 
+import OrderTrackingPage from "./pages/OrderTrackingPage";
+import ClientProfilePage from "./pages/ClientProfilePage";
 
 
 
@@ -56,6 +58,7 @@ function App() {
     <Router>
       <Navbar cart={cart} />
       <Routes>
+        {/* Public routes */}
         <Route
           path="/"
           element={
@@ -71,25 +74,38 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
         <Route path="/reset-password" element={<ResetPasswordPage />} />
-
-
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/profiles/:username" element={<PublicProfile />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/category/woodworking" element={<WoodworkingCategoryPage />} />
         <Route path="/category/metalworking" element={<MetalworkingCategoryPage />} />
         <Route path="/category/textile" element={<TextileCategoryPage />} />
-        <Route path="/" element={<CraftsmenListPage />} />
         <Route path="/craftsmen" element={<CraftsmenListPage />} />
         <Route path="/craftsman/:id" element={<CraftsmanProfilePage />} />
         <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
-        <Route path="/HireSignup" element={<HireSignup />} />
+        <Route path="/HireSignUp" element={<HireSignUp />} />
         <Route path="/HireLogin" element={<HireLogin />} />
         <Route path="/terms" element={<TermsConditionPage />} />
         <Route path="/admin-login" element={<AdminLoginPage />} />
-        <Route path="/hire" element={<HireCraftsmanPage />} />
         <Route path="/services" element={<ServicesPage />} />
-        </Routes>
+        <Route path="/no-shop" element={<NoShopPage />} />
+        <Route path="/orders" element={<OrderTrackingPage />} />
+        <Route path="/profile" element={<ClientProfilePage />} /> 
+
+
+
+
+
+        {/* ✅ Protected hire route */}
+        <Route
+          path="/hire"
+          element={
+            <ProtectedRoute>
+              <HireCraftsmanPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
