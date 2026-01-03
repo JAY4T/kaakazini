94d7dfc62ad5cc31dcb840d82b27c9d227081fd1
rotieno <rabinyitzahck@gmail.com>
2026-01-03 12:00:27 +0300

    refactor: modify the navbar to match the brand colors

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/kazini.png';
+// import logo from '../assets/kazini.png';
+import logo from '../assets/kaz.svg';
+
 
 function Navbar({ cart }) {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -32,7 +34,10 @@ function Navbar({ cart }) {
             height="80"
             style={{ maxHeight: '80px', width: 'auto' }}
             className="d-inline-block align-text-top"
-          />
+          /> 
+           <span className="ms-2 fw-bold">
+              KAAKAZINI
+           </span>
         </Link>
 
         {/* Mobile menu button */}
@@ -46,11 +51,11 @@ function Navbar({ cart }) {
         </button>
 
         <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto d-flex align-items-center">
+          <ul className="navbar-nav ms-auto d-flex align-items-center gap-3">
 
             {/* Craftsmen */}
             <li className="nav-item">
-              <Link className="nav-link" to="/craftsmen" onClick={handleLinkClick}>
+              <Link className="nav-link fs-5" to="/craftsmen" onClick={handleLinkClick}>
                 Craftsmen
               </Link>
             </li>
@@ -62,7 +67,7 @@ function Navbar({ cart }) {
               style={{ cursor: "pointer" }}
             >
               <span
-                className="nav-link d-flex align-items-center"
+                className="nav-link fs-5 d-flex align-items-center"
                 style={{ whiteSpace: "nowrap" }}
               >
                 Services
@@ -121,12 +126,12 @@ function Navbar({ cart }) {
 
             {/* Admin login */}
             <li className="nav-item">
-              <Link className="nav-link" to="/admin-login" onClick={handleLinkClick}></Link>
+              <Link className="nav-link fs-5" to="/admin-login" onClick={handleLinkClick}></Link>
             </li>
 
             {/* Login */}
             <li className="nav-item">
-              <Link className="nav-link" to="/login" onClick={handleLinkClick}>
+              <Link className="nav-link fs-5" to="/login" onClick={handleLinkClick}>
                 Login
               </Link>
             </li>
@@ -135,7 +140,7 @@ function Navbar({ cart }) {
             <li className="nav-item">
               <Link
                 to="/signup"
-                className="btn btn-green-solid ms-2 rounded-pill"
+                className="btn fs-5 btn-yellow-solid ms-2 rounded-pill"
               >
                 Join Now
               </Link>
